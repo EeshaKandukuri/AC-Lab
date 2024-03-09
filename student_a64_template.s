46d74a5052c89e4ec4c9a4991a18466ddc82f8ba
EeshaKandukuri <kandukuri.eesha@gmail.com>
2024-03-09 02:20:26 +0000

    hgello

--- student_a64_template.s
+++ student_a64_template.s
@@ -1,5 +1,5 @@
   // This is the general format of an assembly-language program file.
-    // Written by: REPLACE THIS WITH YOUR NAME AND UT EID
+    // Written by: EESHA KANDUKURI EK22964
     .arch armv8-a
     .text
     // Code for all functions go here.
@@ -20,11 +20,58 @@ ntz:
     // (STUDENT TODO) Code for ntz goes here.
     // Input parameter n is passed in X0
     // Output value is returned in X0.
-    ret
+
+    LSL X1, X0, #1
+    ORR X0, X0, X1
+    LSL X1, X0, #2
+    ORR X0, X0, X1
+    LSL X1, X0, #4
+    ORR X0, X0, X1
+    LSL X1, X0, #8
+    ORR X0, X0, X1
+    LSL X1, X0, #16
+    ORR X0, X0, X1
+    LSL X1, X0, #32
+    ORR X0, X0, X1
+
+    MVN X0, X0
+
+    ANDS X1, X0, #0X5555555555555555
+    LSR X2, X0, #1
+    ANDS X3, X2, #0X5555555555555555
+    ADD X0, X1, X3
+
+    ANDS X1, X0, #0X3333333333333333
+    LSR X2, X0, #2
+    ANDS X3, X2, #0X3333333333333333
+    ADD X0, X1, X3
+
+    ANDS X1, X0, #0X0F0F0F0F0F0F0F0F
+    LSR X2, X0, #4
+    ANDS X3, X2, #0X0F0F0F0F0F0F0F0F
+    ADD X0, X1, X3
+
+    ANDS X1, X0, #0X00FF00FF00FF00FF
+    LSR X2, X0, #8
+    ANDS X3, X2, #0X00FF00FF00FF00FF
+    ADD X0, X1, X3
+
+    ANDS X1, X0, #0X0000FFFF0000FFFF
+    LSR X2, X0, #16
+    ANDS X3, X2, #0X0000FFFF0000FFFF
+    ADD X0, X1, X3
+
+    ANDS X1, X0, #0X00000000FFFFFFFF
+    LSR X2, X0, #32
+    ANDS X3, X2, #0X00000000FFFFFFFF
+    ADD X0, X1, X3
+
+    ANDS X0, X0, #0X000000000000007F
+    
+    ret 
 
     .size   ntz, .-ntz
     // ... and ends with the .size above this line.
-
 
 
     // Every function starts from the .align below this line ...
@@ -36,6 +83,50 @@ ntz:
 aiken_to_long:
     // Input parameter buf is passed in X0
     // Output value is returned in X0.
+    
+    //pseudocode 
+    //0-4 stay same 
+    //val - 6 = curr
+
+    SUBS X7, X7, X7
+    MOVZ X3, #60
+    b .do_while_1
+
+    .do_while_1:
+        LSR X4, X0, X3
+        ANDS X4, X4, #0XF
+
+        CMP X4, #0xB
+        b.ge .Conversion
+
+        CMP X4, #4 
+        b.le .Continue
+
+        CMP X4, #0XB
+        b.lt .Error
+
+    .Continue: 
+        LSL X2, X7, #3
+        LSL X5, X7, #1
+        ADDS X7, X2, X5
+        ADDS X7, X7, X4
+
+        SUBS X3, X3, #4
+        CMP X3, #-4
+        b.eq .done
+        b .do_while_1 
+    .Conversion:
+        SUBS X4, X4, #6
+        b .Continue
+    .Error:
+        SUBS X7, X7, X7
+        SUBS X7, X7, #1
+        b .done
+    .done:
+        SUBS X0, X0, X0
+        ADDS X0, X0, X7
+        ret
+
     ret
 
     .size   aiken_to_long, .-aiken_to_long
@@ -53,6 +144,76 @@ unicode_to_UTF8:
     // (STUDENT TODO) Code for unicode_to_UTF8 goes here.
     // Input parameter a is passed in X0; input parameter utf8 is passed in X1.
     // There are no output values.
+
+    //using a in binary 
+
+    CMP X0, #2097152
+    b.ge .OUT_OF_RANGE
+
+    CMP X0, #65536
+    b.ge .4_BYTES
+
+    CMP X0, #2048
+    b.ge .3_BYTES
+
+    CMP X0, #128
+    b.ge .2_BYTES
+    b.lt .1_BYTE
+
+
+    .1_BYTE:
+        STUR X0, [X1]
+    .2_BYTES:
+        MOVZ X2, #0XC0
+        MOVZ X3, #0X80
+
+        LSR X4, X0, #6
+        LSR X5, X4, #5
+        ORR X2, X2, X4
+        ORR X3, X3, X5
+        STUR X2, [X1]
+        STUR X3, [X1, #8]
+    .3_BYTES:
+        MOVZ X2, #0XE0
+        MOVZ X3, #0X80
+        MOVZ X6, #0X80
+
+        LSR X4, X0, #6
+        LSR X5, X4, #5
+        LSR X7, X5, #5
+        ORR X2, X2, X4
+        ORR X3, X3, X5
+        ORR X6, X6, X7
+        STUR X2, [X1]
+        STUR X3, [X1, #8]
+        STUR X6, [X1, #16]
+
+    .4_BYTES:
+        MOVZ X2, #0XF0
+        MOVZ X3, #0X80
+        MOVZ X6, #0X80
+        MOVZ X8, #0X80
+
+        LSR X4, X0, #6
+        LSR X5, X4, #5
+        LSR X7, X5, #5
+        LSR X9, X7, #5
+        ORR X2, X2, X4
+        ORR X3, X3, X5
+        ORR X6, X6, X7
+        ORR X8, X8, X9
+        STUR X2, [X1]
+        STUR X3, [X1, #8]
+        STUR X6, [X1, #16]
+        STUR X8, [X1, #24]
+
+    .OUT_OF_RANGE:
+        MOVZ X2, 0XFF
+
+        STUR X2, [X1]
+        STUR X2, [X1, #8]
+        STUR X2, [X1, #16]
+        STUR X2, [X1, #24]
 
     ret
     .size   unicode_to_UTF8, .-unicode_to_UTF8
